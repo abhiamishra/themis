4cc0ba442da629d180ae5f0eeb4e5a1f2f5aad57
abhiamishra <abhiamishra0@gmail.com>
2022-09-24 11:46:46 -0500

    Connection from web crawler to backend

--- opinion.py
+++ opinion.py
@@ -19,6 +19,7 @@ class OpinionBuilder:
     def add_justice(self, name: str, opinion: str):
         self.dict["justices"].append({"name": name, "opinion": opinion})
 
+<<<<<<< HEAD
     def __str__(self) -> str:
         print("---------- START ---------------")
         print(self.dict["shortsum"])
@@ -39,3 +40,25 @@ class OpinionBuilder:
 
 def insert_opinion(opinion: OpinionBuilder):
     opinions_col.insert_one(opinion.dict)
+=======
+
+def insert_opinion(opinion: OpinionBuilder):
+    opinions_col.insert_one(opinion.dict)
+
+
+'''
+example usage
+
+o = OpinionBuilder()
+o.set_date(datetime.datetime(2022, 6, 30))
+o.add_justice("Ruth Bader Ginsburg", "majority")
+o.dict["title"] = "Someone v. Someone else"
+o.dict["docket"] = "22-998"
+o.dict["summary"] = "Summary test"
+o.dict["history"] = "Summary test"
+o.dict["link"] = "https://www.supremecourt.gov/opinions/21pdf/21-954_7l48.pdf"
+o.dict["category"] = "jobs"
+
+insert_opinion(o)
+'''
+>>>>>>> 78173879f233eff5cd04f7fd7eb474213f849481
